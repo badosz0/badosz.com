0a4c3c9a05d1fb2700ef8d2ec3e19391c862cb7e
Bartosz Król <badoszk@gmail.com>
2020-10-13 14:57:45 +0200

    json support

--- src/endpoints/homepage.ts
+++ src/endpoints/homepage.ts
@@ -5,13 +5,13 @@ export = (app: Application, config: any) => {
     app.get("/:id?/:sub?", async (req, res) => {
         
         const id = req.params.id
+        const sub = req.params.sub
 
         if (id) {
             switch (id) {
                 case "home":
                 case "projects":
                 case "contact":
-                case "blog":
                     return res
                         .status(200)
                         .render(path.join("../public/homepage/index.ejs"), { 
@@ -20,6 +20,12 @@ export = (app: Application, config: any) => {
                             projects: config.projects,
                             contacts: config.contacts
                         })
+                case "home.json":
+                    return res.status(200).json(config.timeline)
+                case "projects.json":
+                    return res.status(200).json(config.projects)
+                case "contact.json":
+                    return res.status(200).json(config.contacts)
                 default: 
                     if (config.redirect[id]) {
                         res.redirect(config.redirect[id])
