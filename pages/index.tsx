8a10449e3c084b8048e695d48247899b1b314194
badosz0 <30699779+badosz0@users.noreply.github.com>
2021-02-28 22:31:53 +0100

    useless imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,6 @@ import Layout from "../components/Layout"
 export default function Home() {
     return (
         <>
-            {/* <div className="text-green-500 hover:text-red-500">Hello World</div>
-            <Link href="/test">
-                <a>test</a>
-            </Link> */}
-
             {/* <div className="text-base p-4"> 
                 Coffee <FontAwesomeIcon icon={faCoffee}/>
             </div> */}
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from "@fortawesome/free-solid-svg-icons"
 import Layout from "../components/Layout"
 
 export default function Projects() {
