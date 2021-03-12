00902e66fa64c285f8c7e26511a4f25b11425669
badosz0 <30699779+badosz0@users.noreply.github.com>
2021-03-12 20:27:02 +0100

    fix conflicts

--- src/utils/logger.ts
+++
@@ -1,9 +0,0 @@
-const RESET = "\u001b[0m"
-
-export const log_types: {[type: string]: string} = {
-    "info": "\u001b[32m"
-}
-
-export function log(type: string, message: string): void {
-    console.log(`[${log_types[type]}${type.toUpperCase()}${RESET}  | ${(new Date()).toLocaleString()}]: ${log_types[type]}${message}${RESET}`)
-}
