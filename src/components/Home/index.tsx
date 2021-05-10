5a2b270df6dcea5b363363b64e080cda4f9d802a
badosz0 <30699779+badosz0@users.noreply.github.com>
2021-05-10 22:17:09 +0200

    links

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,17 @@
-import { Container } from "~/components/ui/Container";
+import {
+	faDiscord,
+	faGithub,
+	faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { Container } from "../ui/Container";
+import { Contact } from "../ui/Contact";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 
 export function HomePage() {
 	return (
 		<Container>
 			<div className="flex justify-center">
-				<div className="flex flex-col mt-10">
+				<div className="flex flex-col mt-10 space-y-14">
 					<div className="text-main text-center">
 						<div className="text-5xl font-extrabold">
 							Bartosz Król
@@ -16,7 +23,15 @@ export function HomePage() {
 						</div>
 					</div>
 
-					<div className=""></div>
+					<div className="flex justify-center space-x-4 border-t-4 border-b-4 py-2 border-light">
+						<Contact link="/github" icon={faGithub} />
+						<Contact
+							link="mailto:m@badosz.com"
+							icon={faEnvelopeSquare}
+						/>
+						<Contact link="/twitter" icon={faTwitter} />
+						<Contact link="/discord" icon={faDiscord} />
+					</div>
 				</div>
 			</div>
 		</Container>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/app";
 import "~/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
