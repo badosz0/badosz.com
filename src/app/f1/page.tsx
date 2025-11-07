99065b613b85c9994cfe2280b307d284e38ccae1
Bartosz Król <m@badosz.com>
2025-11-07 10:06:32 +0100

    chore: add fp

--- src/app/f1/page.tsx
+++ src/app/f1/page.tsx
@@ -11,6 +11,9 @@ const SESSIONS: Record<string, string> = {
   qualy: 'Qualifying',
   sprintQualy: 'Sprint Qualifying',
   sprintRace: 'Sprint',
+  fp1: 'Free Practice 1',
+  fp2: 'Free Practice 2',
+  fp3: 'Free Practice 3',
 };
 
 const COUNTRY_MAP: Record<string, string> = {
